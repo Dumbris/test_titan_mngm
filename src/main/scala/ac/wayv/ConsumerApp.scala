2ba7681a1d978edb09c7799a293e05a87a2b4dbf
Algis Dumbris <a.dumbris@gmail.com>
2014-08-20 19:53:05 +0400

    Indexes now works ok

--- src/main/scala/ac/wayv/ConsumerApp.scala
+++ src/main/scala/ac/wayv/ConsumerApp.scala
@@ -1,12 +1,12 @@
 package ac.wayv.app
 
-import com.thinkaurelius.titan.core.{TitanLabel, TitanKey, TitanFactory}
+import com.thinkaurelius.titan.core.{TitanFactory}
 import org.apache.commons.configuration.BaseConfiguration
 
 import scala.collection.JavaConversions._
 
 import com.thinkaurelius.titan.core.{PropertyKey, TitanFactory}
-import com.thinkaurelius.titan.core.schema.{ConsistencyModifier, TitanGraphIndex}
+import com.thinkaurelius.titan.core.schema.{SchemaAction, ConsistencyModifier, TitanGraphIndex}
 import com.tinkerpop.blueprints.Vertex
 import org.apache.commons.configuration.BaseConfiguration
 import com.thinkaurelius.titan.graphdb.blueprints.TitanBlueprintsGraph
@@ -14,7 +14,7 @@ import com.tinkerpop.blueprints.Direction
 
 object ConsumerApp extends App {
   val g = TitanFactory.open("/home/algis/workspace/test_titan_mngm_2/conf/titan-cassandra-embedded-es.properties")
-
+  def uuid = java.util.UUID.randomUUID.toString
   createIndexes()
 
   def createIndexes() : Unit = {
@@ -27,26 +27,32 @@ object ConsumerApp extends App {
     mgmt.setConsistency(namei, ConsistencyModifier.LOCK)
     */
     //println(mgmt.getPropertyKey("uid"))
-    val key_uid = mgmt.makePropertyKey("uid").dataType(classOf[String]).make()
-    val uid_index : TitanGraphIndex = mgmt.buildIndex("uid", classOf[Vertex]).addKey(key_uid).unique().buildCompositeIndex()
+    mgmt.getGraphIndexes(classOf[Vertex]).map(_.getName) foreach println
+    mgmt.updateIndex("uid", SchemaAction.REMOVE_INDEX)
+    /*
+    val key_uid = mgmt.makePropertyKey("uid2").dataType(classOf[String]).make()
+    val uid_index : TitanGraphIndex = mgmt.buildIndex("uid2", classOf[Vertex]).addKey(key_uid).unique().buildCompositeIndex()
     mgmt.setConsistency(uid_index, ConsistencyModifier.LOCK)
     val key_title = mgmt.makePropertyKey("title").dataType(classOf[String]).make()
     val key_description = mgmt.makePropertyKey("description").dataType(classOf[String]).make()
     mgmt.buildIndex("texts", classOf[Vertex]).addKey(key_title).addKey(key_description).buildMixedIndex(INDEX_NAME)
-    mgmt.commit()
+    mgmt.commit()*/
   }
 
   val juno = g.addVertex(null)
-  juno.setProperty("name", "juno")
+  juno.setProperty("uid", uuid)
+  juno.setProperty("title", "test12")
+  juno.setProperty("description", uuid)
   val jupiter = g.addVertex(null)
-  jupiter.setProperty("name", "jupiter")
+  jupiter.setProperty("uid", uuid)
+  jupiter.setProperty("title", "test24")
+  jupiter.setProperty("description", uuid)
   val merried = g.addEdge(null, juno, jupiter, "married")
-  //Returns all defined keys in the graph
-  for (x <- g.getTypes(classOf[TitanKey]))
-    println(x)
-  //Returns all defined labels in the graph
-  for (x <- g.getTypes(classOf[TitanLabel]))
-    println(x)
-  for (x <- juno.query.labels("married").vertices)
-    println(x.getProperty("name"))
+  g.commit()
+  g.indexQuery("texts", s"v.title:(test*)").vertices().map(_.getElement).foreach((v : Vertex) => {
+    println("-------------------")
+    println(v.getProperty("uid"))
+    println(v.getProperty("title"))
+    println(v.getProperty("description"))
+  })
 }
